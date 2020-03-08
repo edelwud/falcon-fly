6536447695621b4ddaecfd3b33abfe94ef2e2c4f
Diman <vector_diman@mail.ru>
2020-03-08 13:20:02 +0300

    Import JavaFx Library

---
+++ src/com/falconfly/MainMenu.java
@@ -0,0 +1,12 @@
+package com.falconfly;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainMenu  {
+}
