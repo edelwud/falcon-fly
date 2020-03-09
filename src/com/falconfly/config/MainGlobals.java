115d118d6a7f9f4efb93cc88006e006932571b93
Diman <vector_diman@mail.ru>
2020-03-09 10:46:54 +0300

    global config added

---
+++ src/com/falconfly/config/MainGlobals.java
@@ -0,0 +1,6 @@
+package com.falconfly.config;
+
+public class MainGlobals {
+    public static int WIDTH = 1366;
+    public static int HEIGHT = 768;
+}
--- src/com/falconfly/menu/Settings.java
+++ src/com/falconfly/menu/Settings.java
@@ -1,5 +1,6 @@
 package com.falconfly.menu;
 
+import com.falconfly.config.MainGlobals;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -19,7 +20,7 @@ public class Settings {
         messageOne.setAlignment(Pos.CENTER);
         StackPane layoutSettings = new StackPane();
         layoutSettings.getChildren().add(messageOne);
-        sceneSettings = new Scene(layoutSettings,1366,768);
+        sceneSettings = new Scene(layoutSettings,MainGlobals.WIDTH,MainGlobals.HEIGHT);
         windowMain.setScene(sceneSettings);
         //windowMain.setMaximized(true);
         windowMain.setTitle("Settings");
