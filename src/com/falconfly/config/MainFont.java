892c2094545f5a70b18d3ce8500d580572f1f04b
Diman <vector_diman@mail.ru>
2020-03-09 10:57:33 +0300

    global fonts added

---
+++ src/com/falconfly/config/MainFont.java
@@ -0,0 +1,31 @@
+package com.falconfly.config;
+
+
+import com.sun.tools.javac.Main;
+import javafx.scene.text.Font;
+
+import java.util.Map;
+
+public class MainFont {
+   private Map<String, Font> fonts;
+   public static MainFont instance;
+
+   public MainFont() {
+       instance = this;
+   }
+   public void addFont(String fontName, double size) {
+       try {
+           Font tempFont = new Font(fontName, size);
+           fonts.put(fontName, tempFont);
+       } catch (Exception e) {
+            e.fillInStackTrace();
+       }
+   }
+   public Font getFont(String fontName) {
+       return fonts.get(fontName);
+   }
+
+   public static MainFont getInstance() {
+       return instance;
+   }
+}
