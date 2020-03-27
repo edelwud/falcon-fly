209efcac9de98ee30a3640a35c19a5c9df33d7b2
qkay111 <qkay111@gmail.com>
2020-03-27 16:48:59 +0300

    Few changes with apply and back buttons in settings and some other changes

--- src/com/falconfly/menu/Settings.java
+++ src/com/falconfly/menu/Settings.java
@@ -4,16 +4,25 @@ import com.falconfly.config.MainFont;
 import com.falconfly.config.MainGlobals;
 import com.falconfly.config.MainMusic;
 import com.falconfly.engine.main.Main;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -29,7 +38,7 @@ public class Settings {
     private int restoreHEIGHT = MainGlobals.HEIGHT;
 
     MainEnvironmentLoader environmentLoader;
-    MenuStorageLoader storageLoader;
+    MenuStorageLoader storageLoader = new MenuStorageLoader();
 
     MainFont fonts;
     MainMusic music;
@@ -56,6 +65,123 @@ public class Settings {
         scene = sceneSettings;
         stage = windowMain;
 
+        Dimension screenSize = Toolkit.getDefaultToolkit ().getScreenSize ();
+        int resHeight = screenSize.height;
+        int resWidth  = screenSize.width;
+
+        BackgroundImage centerBackground = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(10), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label mainBackground = new Label();
+        mainBackground.setMaxSize(resWidth, resHeight);
+        mainBackground.setBackground(new Background(centerBackground));
+
+        BackgroundImage borderTransparentBottomLeftImg = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(5), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label borderTransparentBottomLeft = new Label();
+        borderTransparentBottomLeft.setMaxSize(resWidth, resHeight);
+        borderTransparentBottomLeft.setBackground(new Background(borderTransparentBottomLeftImg));
+
+        ScaleTransition stLabel1 = new ScaleTransition(Duration.seconds(1), borderTransparentBottomLeft);
+        stLabel1.setFromX(1);
+        stLabel1.setFromY(1);
+        stLabel1.setToX(1.03);
+        stLabel1.setToY(1.03);
+        stLabel1.play();
+        ParallelTransition parallelTransition1 = new ParallelTransition(stLabel1);
+        parallelTransition1.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition1.setAutoReverse(true);
+        parallelTransition1.play();
+
+        BackgroundImage borderTransparentUpRightImg = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(6), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label borderTransparentUpRight = new Label();
+        borderTransparentUpRight.setMaxSize(resWidth, resHeight);
+        borderTransparentUpRight.setBackground(new Background(borderTransparentUpRightImg));
+
+        ScaleTransition stLabel2 = new ScaleTransition(Duration.seconds(0.7), borderTransparentUpRight);
+        stLabel2.setFromX(1);
+        stLabel2.setFromY(1);
+        stLabel2.setToX(1.03);
+        stLabel2.setToY(1.03);
+        stLabel2.play();
+        ParallelTransition parallelTransition2 = new ParallelTransition(stLabel2);
+        parallelTransition2.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition2.setAutoReverse(true);
+        parallelTransition2.play();
+
+        BackgroundImage borderMainUpLeftImg = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(3), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label borderMainUpLeft = new Label();
+        borderMainUpLeft.setMaxSize(resWidth, resHeight);
+        borderMainUpLeft.setBackground(new Background(borderMainUpLeftImg));
+
+        ScaleTransition stLabel3 = new ScaleTransition(Duration.seconds(1.4), borderMainUpLeft);
+        stLabel3.setFromX(1);
+        stLabel3.setFromY(1);
+        stLabel3.setToX(1.03);
+        stLabel3.setToY(1.03);
+        stLabel3.play();
+        ParallelTransition parallelTransition3 = new ParallelTransition(stLabel3);
+        parallelTransition3.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition3.setAutoReverse(true);
+        parallelTransition3.play();
+
+        BackgroundImage borderMainBottomLeftImg = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(2), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label borderMainBottomLeft = new Label();
+        borderMainBottomLeft.setMaxSize(resWidth, resHeight);
+        borderMainBottomLeft.setBackground(new Background(borderMainBottomLeftImg));
+
+        ScaleTransition stLabel4 = new ScaleTransition(Duration.seconds(1.2), borderMainBottomLeft);
+        stLabel4.setFromX(1);
+        stLabel4.setFromY(1);
+        stLabel4.setToX(1.03);
+        stLabel4.setToY(1.03);
+        stLabel4.play();
+        ParallelTransition parallelTransition4 = new ParallelTransition(stLabel4);
+        parallelTransition4.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition4.setAutoReverse(true);
+        parallelTransition4.play();
+
+        BackgroundImage borderMainUpRightImg = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(4), resWidth, resHeight,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+        Label borderMainUpRight = new Label();
+        borderMainUpRight.setMaxSize(resWidth, resHeight);
+        borderMainUpRight.setBackground(new Background(borderMainUpRightImg));
+
+        ScaleTransition stLabel5 = new ScaleTransition(Duration.seconds(1.2), borderMainUpRight);
+        stLabel5.setFromX(1);
+        stLabel5.setFromY(1);
+        stLabel5.setToX(1.03);
+        stLabel5.setToY(1.03);
+        stLabel5.play();
+        ParallelTransition parallelTransition5 = new ParallelTransition(stLabel5);
+        parallelTransition5.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition5.setAutoReverse(true);
+        parallelTransition5.play();
+
+        BackgroundImage backButtonBackground = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(1),resWidth * 0.17,resHeight * 0.17,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
 
         buttonSettingsBack = new Button();
         buttonSettingsBack.setOnAction((e)->{
@@ -67,20 +193,55 @@ public class Settings {
                 ex.printStackTrace();
             }
         });
-        buttonSettingsBack.setText("Back");
-        buttonSettingsBack.setPrefSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsBack.setMaxSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsBack.setMinSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsBack.setFont(this.fonts.getFont("BN Jinx"));
+        buttonSettingsBack.setBackground(new Background(backButtonBackground));
+        buttonSettingsBack.setPrefSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
+        buttonSettingsBack.setMaxSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
+        buttonSettingsBack.setMinSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
+
+        buttonSettingsBack.setOnMouseEntered(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), buttonSettingsBack);
+            st.setFromX(1);
+            st.setFromY(1);
+            st.setToX(1.1);
+            st.setToY(1.1);
+            st.play();
+        });
+
+        buttonSettingsBack.setOnMouseExited(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), buttonSettingsBack);
+            st.setToX(1);
+            st.setToY(1);
+            st.play();
+        });
+
+        BackgroundImage applyButtonBackground = new BackgroundImage(
+                new Image(storageLoader.Load("images/Settings").get(0),resWidth * 0.17,resHeight * 0.17,false,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
 
         buttonSettingsApply = new Button();
-        buttonSettingsApply.setText("Apply");
+        buttonSettingsApply.setBackground(new Background(applyButtonBackground));
         buttonSettingsApply.setOnAction(this::handleApply);
-        buttonSettingsApply.setPrefSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsApply.setMaxSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsApply.setMinSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.08);
-        buttonSettingsApply.setFont(this.fonts.getFont("BN Jinx"));
+        buttonSettingsApply.setPrefSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
+        buttonSettingsApply.setMaxSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
+        buttonSettingsApply.setMinSize(MainGlobals.WIDTH * 0.3, MainGlobals.HEIGHT * 0.15);
         buttonSettingsApply.setDisable(true);
+
+        buttonSettingsApply.setOnMouseEntered(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), buttonSettingsApply);
+            st.setFromX(1);
+            st.setFromY(1);
+            st.setToX(1.1);
+            st.setToY(1.1);
+            st.play();
+        });
+
+        buttonSettingsApply.setOnMouseExited(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), buttonSettingsApply);
+            st.setToX(1);
+            st.setToY(1);
+            st.play();
+        });
 
         screenSizes = new ChoiceBox<>();
         screenSizes.setValue(Integer.toString(MainGlobals.WIDTH) + "x" + Integer.toString(MainGlobals.HEIGHT));
@@ -150,7 +311,7 @@ public class Settings {
         verticalSettingsBox.getChildren().addAll(labelMusicSettings, horizontalSettingsBox, labelScreen, screenSizes);
 
         VBox mainSettingsBox = new VBox();
-        mainSettingsBox.setSpacing(25);
+        mainSettingsBox.setSpacing(0);
         mainSettingsBox.getChildren().addAll(buttonSettingsApply, buttonSettingsBack);
 
         BorderPane layout = new BorderPane();
@@ -161,10 +322,14 @@ public class Settings {
 
         layout.setBottom(mainSettingsBox);
         BorderPane.setAlignment(mainSettingsBox, Pos.BOTTOM_RIGHT);
-        BorderPane.setMargin(mainSettingsBox, new Insets(0,0,20,25));
+        BorderPane.setMargin(mainSettingsBox, new Insets(0,0,20,5));
 
+        StackPane root = new StackPane(mainBackground);
+        root.getChildren().addAll(borderTransparentBottomLeft, borderTransparentUpRight,
+                borderMainBottomLeft, borderMainUpLeft, borderMainUpRight);
+        root.getChildren().add(layout);
 
-        sceneSettings = new Scene(layout, MainGlobals.WIDTH, MainGlobals.HEIGHT);
+        sceneSettings = new Scene(root, MainGlobals.WIDTH, MainGlobals.HEIGHT);
         windowMain.setScene(sceneSettings);
         windowMain.setWidth(MainGlobals.WIDTH);
         windowMain.setHeight(MainGlobals.HEIGHT);
